b624403162833c7c9bd3f9fc37e39ed013762d36
Takudzwa <takudzwamaseva020@gmail.com>
2023-07-08 00:04:19 +0200

    Updated scrapeLinkedlnJobs function

--- linkedin-scraper.js
+++ linkedin-scraper.js
@@ -1,61 +1,71 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeLinkedInJobs(jobTitles, url)
-
  {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    // Load job listings
-    await page.waitForSelector('.job-search-card');
-  
-    // Scrap the job listings
-    const jobListings = await page.$$('.job-search-card');
-  
-    // extract info from each job listingf
-    const jobs = [];
-    for (const listing of jobListings) 
+  let jobs = [];
+  let start = 0;
+  let jobListingsCount = 0;
+  let currentJobListings = [];
 
+  do 
+  {
+    await page.waitForSelector('li');
+
+    currentJobListings = await page.$$eval('li', listings => 
     {
-      const job = {};
-  
-      // Job title
-      const jobTitle = await listing.$('.base-search-card__title');
-      job.title = await page.evaluate(element => element.innerText, jobTitle);
-  
-      // Company name
-      const companyName = await listing.$('h4.base-search-card__subtitle a');
-      job.company = await page.evaluate(element => element.innerText, companyName);
-  
-      // Job location
-      const jobLocation = await listing.$('.job-search-card__location');
-      job.location = await page.evaluate(element => element.innerText, jobLocation);
-  
-      // Job link
-      const jobLink = await listing.$('a.base-card__full-link');
-      job.link = await page.evaluate(element => element.href, jobLink);
-  
-      jobs.push(job);
-
-    }
-  
-    await browser.close();
-  
-    return jobs;
-
-  }
-    
-  const jobTitles = process.argv.slice(2).join(' ');
-  const searchUrl = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=${encodeURIComponent(jobTitles)}&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0`;
-  
-  scrapeLinkedInJobs(jobTitles, searchUrl)
-    .then(jobs => {
-      console.log(jobs);
-      console.log(jobs.length);
-      // Process the scraped job data as needed
-    })
-    .catch(error => {
-      console.error('Error:', error);
+      return listings.map(listing => {
+        const job = {};
+        const jobTitle = listing.querySelector('.base-search-card__title');
+        job.title = jobTitle ? jobTitle.innerText.trim() : 'no title data';
+        const companyName = listing.querySelector('h4.base-search-card__subtitle a');
+        job.company = companyName ? companyName.innerText.trim() : 'no company name data';
+        const jobLocation = listing.querySelector('.job-search-card__location');
+        job.location = jobLocation ? jobLocation.innerText.trim() : 'no job location data';
+        const jobLink = listing.querySelector('a.base-card__full-link');
+        job.link = jobLink ? jobLink.href : 'no job link data';
+        return job;
+      });
     });
-  
+
+    jobs = jobs.concat(currentJobListings);
+    jobListingsCount = currentJobListings.length;
+    start += jobListingsCount;
+
+    if (jobListingsCount < 25)
+     {
+      break; 
+    }
+
+    await page.goto(`${url}&start=${start}`, { waitUntil: 'networkidle0' });
+
+    // Delay to allow job listings to load
+    await page.waitForTimeout(2000); // Adjust the delay time as needed
+  } while (jobListingsCount > 0);
+
+  await browser.close();
+
+  return jobs;
+}
+
+async function runScraping() 
+{
+  try
+   {
+    const jobTitles = process.argv.slice(2).join(' ');
+    const searchUrl = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=${encodeURIComponent(jobTitles)}&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit`;
+
+    const jobs = await scrapeLinkedInJobs(jobTitles, searchUrl);
+    console.log(jobs);
+    console.log(jobs.length);
+
+  } catch (error)
+   {
+    console.error('Error:', error);
+  }
+}
+
+runScraping();
